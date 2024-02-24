f40c4e649b9f77b8701df14923a9cacac9d04208
Sachin Jadhav <sp.jadhav10@gmail.com>
2024-02-24 13:09:17 +0530

    code changes

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="SachinJ",
+    author_email="spjadhav10@gmail.com",
+    packages=find_packages()
+)

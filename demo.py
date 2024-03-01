63586b71412520456c2cc3bd69ffd07b42f6c0ad
Sachin Jadhav <sp.jadhav10@gmail.com>
2024-03-01 19:01:16 +0530

    updated

--- demo.py
+++ demo.py
@@ -1,9 +1,17 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+#from us_visa.logger import logging
+#from us_visa.exception import USvisaException
+#import sys
 
+'''
 try:
     a = 1 / "10"
 except Exception as e:
     logging.info(e)
     raise USvisaException(e, sys) from e
+'''
+
+from us_visa.pipline.training_pipeline import TrainPipeline
+
+
+pipline  = TrainPipeline()
+pipline.run_pipeline()
